be027cf694cccc94c45a465371bd2e848ad2eb49
andrewSC <andrew@crerar.io>
2017-11-09 14:53:47 -0500

    Better filtering and search for aur fonts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import fnmatch
+import json
 import os
 import re
 import subprocess
@@ -22,7 +23,10 @@ def exec_cmd(cmd):
 
     return proc_result.stdout.decode('UTF-8')
 
-def generate_font_dict(fonts):
+def fetch_repo_fonts():
+    cmd = ['pkgfile', '-rv', '/usr/share/fonts/.*\.(ttf)']
+    fonts = exec_cmd(cmd).split('\n')
+
     repo_fonts = {}
     for font in fonts:
         if font:
@@ -50,15 +54,76 @@ def fetch_google_fonts():
                     break
     return fonts
 
+"""
+    For the first font in every folder within the google fonts repo, use `fc-query` to get the expected package name
+    of the font. Store this name with the google fonts relative path for further processing
+"""
+def fetch_google_font_names(flagged_google_fonts):
+    font_names = {}
+    for root, dirs, files in os.walk('fonts'):
+        if files:
+            for file in files:
+                if fnmatch.fnmatch(file, '*.ttf'):
+                    cmd = ['fc-query', '-f', '%{family[0]|downcase|translate( ,-)}\n', os.path.abspath(root + '/' + file)]
+                    if root not in flagged_google_fonts:
+                        font_names[root] = exec_cmd(cmd).split('\n')[1]
+                        break
+
+    return font_names
+
+
+"""
+    font_names: key, value pairs representing the relative path in the google fonts repo and the name of the font for each folder.
+"""
+def fetch_aur_fonts(font_names):
+    fonts = {}
+    for font_path, name in font_names.items():
+        cmd = ['aursearch', '-vr', name]
+        result = json.loads(exec_cmd(cmd))
+        if not result:
+            print('ERROR: Expected response from AUR to contain 0 or more results. Response was empty.')
+            print(f"Result: {result}, Font path: {font_path}, Font name: {name}")
+            continue
+        else:
+            result = result[0]  #  We direct index here because the list always has one result which is the json obj itself
+
+        if int(result['resultcount']) == 0:
+            fonts[font_path] = name + '-fonts'
+        else:
+            # NOTE: There are actually search results for the font name, let's list out the packages that came back
+            font_packages = []
+            potential_matches = False
+            for package in result['results']:
+                pkg_name = package['Name']
+
+                #  NOTE: Only consider the package if the name of the font is actually part of the package name itself
+                if name in pkg_name and any(keyword in pkg_name for keyword in ['ttf', 'font']):
+                    font_packages.append(pkg_name)
+                    potential_matches = True
+
+            if not potential_matches:
+                fonts[font_path] = name + '-fonts'
+            else:
+                print(f"[{font_path}] potentially satisfied by {font_packages}")
+
+    return fonts
+
+
 if __name__ == "__main__":
     # cmd = ['pkgfile', '-u']  # requires su, maybe leave that to the user?
 
-    cmd = ['pkgfile', '-rv', '/usr/share/fonts/.*\.']
-    result = exec_cmd(cmd)
-    font_dict = generate_font_dict(result.split('\n'))
+    repo_font_dict = fetch_repo_fonts()
     google_font_dict = fetch_google_fonts()
 
-    for google_font_name, path in google_font_dict.items():
-        if google_font_name in font_dict:
-            print(f"Collision: [{google_font_dict[google_font_name]}] is provided by package [{font_dict[google_font_name]}]")
+    flagged_google_fonts = set()  #  Contains all the [extra] and [community] packages that provide fonts that google-fonts also provides (but will be removed in the PKGBUILD).
 
+    for google_font_name, path in google_font_dict.items():
+        if google_font_name in repo_font_dict:
+            # print(f"Collision: [{google_font_dict[google_font_name]}] is provided by package [{repo_font_dict[google_font_name]}]")
+            flagged_google_fonts.add(google_font_dict[google_font_name])
+
+    font_names = fetch_google_font_names(flagged_google_fonts)  #  For every font folder in the google fonts repo, 
+    autogen_font_names = fetch_aur_fonts(font_names)
+    print('-------------------------------------------------------------------')
+    for font_path, name in autogen_font_names.items():
+        print(f"[{name}] virtually provides [{font_path}]")
